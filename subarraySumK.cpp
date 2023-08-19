2d81e62067213b4399e04282c8289936c70fcd05
Chong Xern <104547209+ChongXern@users.noreply.github.com>
2023-08-20 01:16:56 +0800

    Update subarraySumK.cpp

--- subarraySumK.cpp
+++ subarraySumK.cpp
@@ -22,23 +22,55 @@ public:
 		return false;
 	}
     int subarraySum(vector<int>& nums, int k) {
-
+	    int count = 0;
+	    int start = 0;
+	    int end = 1;
+	    currSum = nums[0];
+	    while (start < nums.size() - 1 && end < nums.size() - 1) {
+		    cout << currSum << endl;
+		    if (currSum == k) {
+			    count++;
+			    currSum -= nums[start];
+			    start++;
+			    end++;
+			    currSum += nums[end];
+		    } 
+		    else if (currSum < k) {
+			    end++;
+			    currSum += nums[end];
+		    } else {
+			    currSum -= nums[start];
+			    start++;
+		    }
+	    }
+	    return count;
     }
 };
 
 
 vector<int> vectorFromArray(int* array, int size) {
+	vector<int> vector;
+	for (int i = 0; i < size; i++) {
+		vector.push_back(array[i];
+	}
+	return vector;
 }
 
 int main() {
 	int numsSize, k;
 	cout << "Input size of array: ";
 	cin >> numsSize;
-	int *nums = new int[numsSize];
+	int *numsArr = new int[numsSize];
 	cout << "Input nums:" << endl;
 	int num;
 	for (int i = 0; i < numsSize; i++) {
 		cin >> num;
-		nums[i] = num;
+		numsArr[i] = num;
 	}
+	cout << "Input k: ";
+	cin >> k;
+	vector<int> nums = vectorFromArray(numsArr, numsSize);
+	Solution solution;
+	int output = solution.subarraySum(nums, k);
+	cout << "Total number of subarrays whose sum equals to k: " << output << endl;
 }
