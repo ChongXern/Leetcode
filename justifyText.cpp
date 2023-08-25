22276a371b6d2e8f8346a68d267aa6ccf1e8d81e
ChongXern <chongxern@gmail.com>
2023-08-25 11:01:01 +0800

    Attempted justifyText.cpp

--- justifyText.cpp
+++ justifyText.cpp
@@ -15,29 +15,51 @@ public:
         vector<string> output;
         vector<string> currWords;
         int currLetterCount = 0;
+        int currWordsSize = 0;
         int countSpace;
+        string line = "";
         for (int i = 0; i < words.size(); i++) {
-            if (2 * currLetterCount + words[i].size() > maxWidth) {
-                countSpace = (maxWidth - currLetterCount) / currWords.size();
-				string space = "";
-				for (int i = 0; i < countSpace; i++) {
-					space += ' ';
-				}
-				string line = "";
-				for (int i = 0; i < currWords.size() - 1; i++) {
-					line += currWords[i];
-					line += space;
-				}
-				line += currWords[currWords.size() - 1];
-				output.push_back(line);
-				currLetterCount = 0;
-				currWords.clear();
-				countSpace = 0;
+            if (2 * currLetterCount + words[i].size() > maxWidth + 6) {
+                cout << currLetterCount << endl;
+                if (currWordsSize == 1) {
+                    string space(maxWidth - currWords[0].size(), ' ');
+                    line += currWords[0];
+                    line += space;
+                    output.push_back(line);
+                    line = "";
+                    currLetterCount = 0;
+                    currWordsSize = 0;
+                    currWords.clear();
+                    countSpace = 0;
+                    continue;
+                }
+                countSpace = (maxWidth - currLetterCount) / (currWordsSize - 1);
+                string space(countSpace, ' ');
+                for (int i = 0; i < currWordsSize - 1; i++) {
+                    line += currWords[i];
+                    line += space;
+                }
+                line += currWords[currWordsSize - 1];
+                output.push_back(line);
+                line = "";
+                currLetterCount = 0;
+                currWordsSize = 0;
+                currWords.clear();
+                countSpace = 0;
             }
             currLetterCount += words[i].size();
             currWords.push_back(words[i]);
+            currWordsSize++;
         }
-		return output;
+        string finalLine = "";
+        for (int i = 0; i < currWords.size(); i++) {
+            finalLine += currWords[i];
+            finalLine += " ";
+        }
+        string space(maxWidth - finalLine.size(), ' ');
+        finalLine += space;
+        output.push_back(finalLine);
+        return output;
     }
 };
 
